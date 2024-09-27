206562d39af8016c958feede9a0a7b622382bbc7
Mayankkaushik1312001 <36242268+Mayankkaushik1312001@users.noreply.github.com>
2024-09-27 06:58:45 +0000

    2nd

--- 01_JavaScript_Basics/main.js
+++ 01_JavaScript_Basics/main.js
@@ -1,1 +1,7 @@
-console.log("Mayank")
+const empId=[23424,5345,345,345];
+var mobileNumber = [7908728032,34535,345345345345,34534653453];
+let idNumber = [345324,45345345345,345345345,345];
+
+console.table([empId,mobileNumber,idNumber])
+
+
